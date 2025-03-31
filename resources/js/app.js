f71c8c376581a3ca549a2c9ba7f4f7b99c5f4b05
B00152361 Darryl Terrobias <B00152361@mytudublin.ie>
2025-03-31 15:17:40 +0100

    Second Commit - finish first step , successful npm run dev

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,2 @@
 require('./bootstrap');
 
-require('jquery/dist/jquery.js');
-require('bootstrap/dist/js/bootstrap.js');
-require('bootstrap-star-rating/js/star-rating.js');
-global.$ = global.jQuery = require('jquery');
